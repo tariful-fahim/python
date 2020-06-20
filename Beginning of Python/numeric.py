d37955cb3347a471f9245639fc50707e9740e86b
Fahim <61591757+tariful-fahim@users.noreply.github.com>
2020-06-21 05:52:24 +0600

    Working with Numeric Data

---
+++ Beginning of Python/numeric.py
@@ -0,0 +1,28 @@
+#Data Type
+num = 2
+num1 = 3.4
+msg = 'NICE'
+print(type(num))
+print(type(num1))
+print(type(msg))
+
+#division
+print(3/2) #it shows the point value after decimal
+
+#floor divison
+print(3//2)
+
+#Exponent
+print(3 ** 2)
+
+#round method
+print(round(3.499999,4)) #round(number, which decimal point number we want to show)
+
+#casting 
+num_1 = '200'
+num_2 = '300'
+print(num_1+num_2)
+print(int(num_1)+int(num_2))
+
+# DIR method
+print(dir(int))
